7c5d2f46d41102ee45f4639b0e095f63c46b1f6d
Bruno Rocha de Lima <brunorocha33@live.com>
2022-09-23 13:27:52 -0300

    logoff

--- consultar_chamado.php
+++ consultar_chamado.php
@@ -26,6 +26,11 @@ require_once 'validar_acesso.php';
       <img src="logo.png" width="30" height="30" class="d-inline-block align-top" alt="">
       App Help Desk
     </a>
+    <ul class="navbar-nav">
+      <li class="nav-item">
+        <a class="nav-link" href="logoff.php">SAIR</a>
+      </li>
+    </ul>
   </nav>
 
   <div class="container">
@@ -59,7 +64,7 @@ require_once 'validar_acesso.php';
 
             <div class="row mt-5">
               <div class="col-6">
-                <a href="home.php" class="btn btn-lg btn-warning btn-block" >Voltar</a>
+                <a href="home.php" class="btn btn-lg btn-warning btn-block">Voltar</a>
               </div>
             </div>
           </div>
